e7451ddbbcd3f5f1afa877b916b05215d2f606bd
Dickon Field <dikonikon@gmail.com>
2016-05-02 22:32:09 +0100

    second commit

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,9 +1,15 @@
 package com.example
 
 import play.api.libs.json._
+import scala.reflect.runtime.universe._
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+
+  	val rawJson = """{"hello": "world", "age": 42}"""
+
+  	val json = Json.parse(rawJson)
+
+    println(s"json: $json")
   }
 }
